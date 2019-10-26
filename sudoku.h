dfea048ad16340b7f9322d694c0e6eb8299afb7d
Maurice Zard <mz2713@vm-shell2.doc.ic.ac.uk>
2019-10-26 14:15:27 +0100

    Second attempt

--- sudoku.h
+++ sudoku.h
@@ -1,2 +1,8 @@
 void load_board(const char* filename, char board[9][9]);
 void display_board(const char board[9][9]);
+bool is_complete(const char board[9][9]);
+bool make_move(const char move_str[], char digit, char board[9][9]); 
+bool save_board(const char* filename, char board[9][9]);
+bool solve_board(char board[9][9], int row = 0, int col = 0);
+bool solve_next(char board[9][9], int row, int col);
+bool is_valid(const char board[9][9], int row, int col, int num);
